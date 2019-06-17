8d599cadd15ef4ca13ea842a5b5e7aa680989768
richardsongo <rgsongo@gmail.com>
2019-06-17 07:07:04 -0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -3,3 +3,24 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    
+class Categories(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    
+class Locations(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Reviews(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Ratings(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+
